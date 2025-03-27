0661c39753ee1a26f64be9331d63e6a7c8e95021
Will Jacknight <jiankaiwang1999@163.com>
2025-03-27 09:37:40 +0800

    remove content class for better noght mode toggling

--- src/App.jsx
+++ src/App.jsx
@@ -48,15 +48,13 @@ function App() {
     <Router>
       <div className={`app ${darkMode ? 'dark-mode' : ''}`}>
         <NavBar onThemeToggle={toggleTheme} />
-        <main className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<ProjectsPage />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:slug" element={<BlogPage />} />
-            <Route path="/about" element={<AboutPage />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
     </Router>
   );
